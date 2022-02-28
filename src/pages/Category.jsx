13ca13c35e8d1f49bd8118496e51ff7089b6822f
Adhithyan S <adhithyanalan@gmail.com>
2022-02-28 11:16:01 +0530

    category

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,11 +1,84 @@
-import React from 'react';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  collection,
+  query,
+  getDocs,
+  where,
+  orderBy,
+  limit,
+} from "firebase/firestore";
+import { db } from "../firebase.config";
+import { toast } from "react-toastify";
+import Spinner from "../components/Spinner";
 
 const Category = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        const listingsRef = collection(db, "listings");
+
+        const q = query(
+          listingsRef,
+          where("type", "==", params.categoryName),
+          orderBy("timestamp", "desc"),
+          limit(10)
+        );
+
+        const querySnap = await getDocs(q);
+
+        const listings = [];
+
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+
+        console.log(listings);
+
+        setListings(listings);
+        setLoading(false);
+      } catch (e) {
+        toast.error("Could not fetch listings");
+      }
+    };
+    fetchListings();
+  }, [params.categoryName]);
+
+  return (
+    <div className="category">
+      <header>
+        <p className="pageHeader">
+          {params.categoryName === "rent"
+            ? "Place for a rent"
+            : "Place for a sale"}
+        </p>
+      </header>
+
+      {loading ? (
+        <Spinner />
+      ) : listings && listings.length > 0 ? (
+        <>
+          <main>
+            <ul className="categoryListings">
+              {listings.map((listing) => (
+                <h3>{listing.data.name}</h3>
+              ))}
+            </ul>
+          </main>
+        </>
+      ) : (
+        <p>No listings for {params.categoryName}</p>
+      )}
+    </div>
+  );
 };
 
 export default Category;
